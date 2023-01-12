7a400ba472d11e6f32205abd9066f2c5ba7862ea
Mask-coins <uton_yayuyo_ics@yahoo.co.jp>
2023-01-12 16:54:36 +0900

    Observationを追加。継続的なアクセスとデータ貯蓄。

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _requires_from_file(filename):
 
 setup(
     name="query_caching_service",
-    version="0.2.2",
+    version="0.2.3",
     description="一度アクセスした資源に何度もアクセスしないためのツールです．",
     author="Mask_coins",
     url="https://github.com/Mask-coins/query_caching_service",
